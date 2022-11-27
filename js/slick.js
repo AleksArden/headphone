48a80549b8973e19e089e2c280edfb58cf2ab9c9
AleksArden <i1723@i.ua>
2022-11-27 23:08:37 +0200

    review 27.11

--- js/slick.js
+++ js/slick.js
@@ -7,10 +7,3 @@ $('.fade').slick({
     arrows: false,
 });
 
-const getRef = x = document.querySelector(x);
-
-console.log(getRef("#slick-slide-control00"));
-
-// {/* <svg class="benefits__icon" width="13" height="13" aria-hidden="true">
-//     <use href="./images/icons.svg#icon-point"></use>
-// </svg> */}
