24080a6a55ee576bd52a7b547695cf8db8335978
AleksArden <i1723@i.ua>
2022-11-27 16:14:52 +0200

    fixed review 27.11

---
+++ js/slick.js
@@ -0,0 +1,16 @@
+$('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+});
+
+const getRef = x = document.querySelector(x);
+
+console.log(getRef("#slick-slide-control00"));
+
+// {/* <svg class="benefits__icon" width="13" height="13" aria-hidden="true">
+//     <use href="./images/icons.svg#icon-point"></use>
+// </svg> */}
